0d6ac3a352ff775a05a0e2582895f2a80e96c068
Johan Tibell <johan.tibell@gmail.com>
2013-12-19 13:08:44 +0100

    Switch Haskell indentation mode

--- lib/haskell.el
+++ lib/haskell.el
@@ -1,7 +1,8 @@
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'haskell-style)
 (global-set-key (kbd "C-c C-h") 'haskell-hoogle)
 (global-set-key (kbd "C-c C-r") 'inferior-haskell-reload-file)
+
 (add-to-list 'completion-ignored-extensions ".hi")
 (require 'haskell-style)
 (require 'ghc-core)
@@ -11,6 +12,9 @@
 
 (eval-after-load "haskell-cabal"
     '(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile))
+
+(eval-after-load "haskell-mode"
+    '(define-key haskell-mode-map (kbd "C-c C-.") 'haskell-sort-imports))
 
 (defun add-ghc-core-keys ()
   (local-set-key (kbd "C-c c") 'ghc-core-clean-buffer))
